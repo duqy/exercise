2394e2b01ba078cb4ebaeb0f4328ca0ea84988ed
dqy <dut2718@163.com>
2018-04-01 21:11:27 +0800

    new file:   Makefile 	new file:   QMainWindow 	new file:   QMainWindow.pro 	new file:   QMainWindow.ui 	new file:   main.cpp 	new file:   main.o 	new file:   mainwindow.cpp 	new file:   mainwindow.h 	new file:   mainwindow.o 	new file:   moc_mainwindow.cpp 	new file:   moc_mainwindow.o 	new file:   moc_predefs.h 	new file:   ui_QMainWindow.h

---
+++ c/QMainWindow/moc_mainwindow.cpp
@@ -0,0 +1,214 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.10.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[21];
+    char stringdata0[424];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 5), // "input"
+QT_MOC_LITERAL(2, 17, 0), // ""
+QT_MOC_LITERAL(3, 18, 3), // "str"
+QT_MOC_LITERAL(4, 22, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(5, 44, 12), // "displayInput"
+QT_MOC_LITERAL(6, 57, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(7, 81, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(8, 105, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(9, 129, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(10, 153, 23), // "on_pushButton_6_clicked"
+QT_MOC_LITERAL(11, 177, 23), // "on_pushButton_7_clicked"
+QT_MOC_LITERAL(12, 201, 23), // "on_pushButton_8_clicked"
+QT_MOC_LITERAL(13, 225, 23), // "on_pushButton_9_clicked"
+QT_MOC_LITERAL(14, 249, 24), // "on_pushButton_10_clicked"
+QT_MOC_LITERAL(15, 274, 24), // "on_pushButton_12_clicked"
+QT_MOC_LITERAL(16, 299, 24), // "on_pushButton_13_clicked"
+QT_MOC_LITERAL(17, 324, 24), // "on_pushButton_14_clicked"
+QT_MOC_LITERAL(18, 349, 24), // "on_pushButton_15_clicked"
+QT_MOC_LITERAL(19, 374, 24), // "on_pushButton_11_clicked"
+QT_MOC_LITERAL(20, 399, 24) // "on_pushButton_16_clicked"
+
+    },
+    "MainWindow\0input\0\0str\0on_pushButton_clicked\0"
+    "displayInput\0on_pushButton_2_clicked\0"
+    "on_pushButton_3_clicked\0on_pushButton_4_clicked\0"
+    "on_pushButton_5_clicked\0on_pushButton_6_clicked\0"
+    "on_pushButton_7_clicked\0on_pushButton_8_clicked\0"
+    "on_pushButton_9_clicked\0"
+    "on_pushButton_10_clicked\0"
+    "on_pushButton_12_clicked\0"
+    "on_pushButton_13_clicked\0"
+    "on_pushButton_14_clicked\0"
+    "on_pushButton_15_clicked\0"
+    "on_pushButton_11_clicked\0"
+    "on_pushButton_16_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  104,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    0,  107,    2, 0x08 /* Private */,
+       5,    1,  108,    2, 0x08 /* Private */,
+       6,    0,  111,    2, 0x08 /* Private */,
+       7,    0,  112,    2, 0x08 /* Private */,
+       8,    0,  113,    2, 0x08 /* Private */,
+       9,    0,  114,    2, 0x08 /* Private */,
+      10,    0,  115,    2, 0x08 /* Private */,
+      11,    0,  116,    2, 0x08 /* Private */,
+      12,    0,  117,    2, 0x08 /* Private */,
+      13,    0,  118,    2, 0x08 /* Private */,
+      14,    0,  119,    2, 0x08 /* Private */,
+      15,    0,  120,    2, 0x08 /* Private */,
+      16,    0,  121,    2, 0x08 /* Private */,
+      17,    0,  122,    2, 0x08 /* Private */,
+      18,    0,  123,    2, 0x08 /* Private */,
+      19,    0,  124,    2, 0x08 /* Private */,
+      20,    0,  125,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->input((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->on_pushButton_clicked(); break;
+        case 2: _t->displayInput((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 3: _t->on_pushButton_2_clicked(); break;
+        case 4: _t->on_pushButton_3_clicked(); break;
+        case 5: _t->on_pushButton_4_clicked(); break;
+        case 6: _t->on_pushButton_5_clicked(); break;
+        case 7: _t->on_pushButton_6_clicked(); break;
+        case 8: _t->on_pushButton_7_clicked(); break;
+        case 9: _t->on_pushButton_8_clicked(); break;
+        case 10: _t->on_pushButton_9_clicked(); break;
+        case 11: _t->on_pushButton_10_clicked(); break;
+        case 12: _t->on_pushButton_12_clicked(); break;
+        case 13: _t->on_pushButton_13_clicked(); break;
+        case 14: _t->on_pushButton_14_clicked(); break;
+        case 15: _t->on_pushButton_15_clicked(); break;
+        case 16: _t->on_pushButton_11_clicked(); break;
+        case 17: _t->on_pushButton_16_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            typedef void (MainWindow::*_t)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::input)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::input(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ c/QMainWindow/ui_QMainWindow.h
@@ -0,0 +1,218 @@
+/********************************************************************************
+** Form generated from reading UI file 'QMainWindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.10.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_QMAINWINDOW_H
+#define UI_QMAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QLabel *my_label;
+    QWidget *layoutWidget;
+    QVBoxLayout *verticalLayout;
+    QPushButton *pushButton;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_9;
+    QPushButton *pushButton_13;
+    QWidget *layoutWidget1;
+    QVBoxLayout *verticalLayout_3;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_7;
+    QPushButton *pushButton_11;
+    QPushButton *pushButton_15;
+    QWidget *layoutWidget2;
+    QVBoxLayout *verticalLayout_4;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_12;
+    QPushButton *pushButton_16;
+    QWidget *layoutWidget3;
+    QVBoxLayout *verticalLayout_2;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_10;
+    QPushButton *pushButton_14;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(487, 246);
+        MainWindow->setMaximumSize(QSize(1677, 1677));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        my_label = new QLabel(centralwidget);
+        my_label->setObjectName(QStringLiteral("my_label"));
+        my_label->setGeometry(QRect(20, 30, 311, 21));
+        layoutWidget = new QWidget(centralwidget);
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
+        layoutWidget->setGeometry(QRect(20, 70, 92, 132));
+        verticalLayout = new QVBoxLayout(layoutWidget);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton = new QPushButton(layoutWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+        pushButton_5 = new QPushButton(layoutWidget);
+        pushButton_5->setObjectName(QStringLiteral("pushButton_5"));
+
+        verticalLayout->addWidget(pushButton_5);
+
+        pushButton_9 = new QPushButton(layoutWidget);
+        pushButton_9->setObjectName(QStringLiteral("pushButton_9"));
+
+        verticalLayout->addWidget(pushButton_9);
+
+        pushButton_13 = new QPushButton(layoutWidget);
+        pushButton_13->setObjectName(QStringLiteral("pushButton_13"));
+
+        verticalLayout->addWidget(pushButton_13);
+
+        layoutWidget1 = new QWidget(centralwidget);
+        layoutWidget1->setObjectName(QStringLiteral("layoutWidget1"));
+        layoutWidget1->setGeometry(QRect(260, 70, 92, 132));
+        verticalLayout_3 = new QVBoxLayout(layoutWidget1);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        pushButton_3 = new QPushButton(layoutWidget1);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+
+        verticalLayout_3->addWidget(pushButton_3);
+
+        pushButton_7 = new QPushButton(layoutWidget1);
+        pushButton_7->setObjectName(QStringLiteral("pushButton_7"));
+
+        verticalLayout_3->addWidget(pushButton_7);
+
+        pushButton_11 = new QPushButton(layoutWidget1);
+        pushButton_11->setObjectName(QStringLiteral("pushButton_11"));
+
+        verticalLayout_3->addWidget(pushButton_11);
+
+        pushButton_15 = new QPushButton(layoutWidget1);
+        pushButton_15->setObjectName(QStringLiteral("pushButton_15"));
+
+        verticalLayout_3->addWidget(pushButton_15);
+
+        layoutWidget2 = new QWidget(centralwidget);
+        layoutWidget2->setObjectName(QStringLiteral("layoutWidget2"));
+        layoutWidget2->setGeometry(QRect(380, 70, 92, 132));
+        verticalLayout_4 = new QVBoxLayout(layoutWidget2);
+        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        verticalLayout_4->setContentsMargins(0, 0, 0, 0);
+        pushButton_4 = new QPushButton(layoutWidget2);
+        pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
+
+        verticalLayout_4->addWidget(pushButton_4);
+
+        pushButton_8 = new QPushButton(layoutWidget2);
+        pushButton_8->setObjectName(QStringLiteral("pushButton_8"));
+
+        verticalLayout_4->addWidget(pushButton_8);
+
+        pushButton_12 = new QPushButton(layoutWidget2);
+        pushButton_12->setObjectName(QStringLiteral("pushButton_12"));
+
+        verticalLayout_4->addWidget(pushButton_12);
+
+        pushButton_16 = new QPushButton(layoutWidget2);
+        pushButton_16->setObjectName(QStringLiteral("pushButton_16"));
+
+        verticalLayout_4->addWidget(pushButton_16);
+
+        layoutWidget3 = new QWidget(centralwidget);
+        layoutWidget3->setObjectName(QStringLiteral("layoutWidget3"));
+        layoutWidget3->setGeometry(QRect(140, 70, 92, 132));
+        verticalLayout_2 = new QVBoxLayout(layoutWidget3);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        pushButton_2 = new QPushButton(layoutWidget3);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+
+        verticalLayout_2->addWidget(pushButton_2);
+
+        pushButton_6 = new QPushButton(layoutWidget3);
+        pushButton_6->setObjectName(QStringLiteral("pushButton_6"));
+
+        verticalLayout_2->addWidget(pushButton_6);
+
+        pushButton_10 = new QPushButton(layoutWidget3);
+        pushButton_10->setObjectName(QStringLiteral("pushButton_10"));
+
+        verticalLayout_2->addWidget(pushButton_10);
+
+        pushButton_14 = new QPushButton(layoutWidget3);
+        pushButton_14->setObjectName(QStringLiteral("pushButton_14"));
+
+        verticalLayout_2->addWidget(pushButton_14);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QStringLiteral("menubar"));
+        menubar->setGeometry(QRect(0, 0, 487, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QStringLiteral("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        my_label->setText(QApplication::translate("MainWindow", "TextLabel", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "1", nullptr));
+        pushButton_5->setText(QApplication::translate("MainWindow", "4", nullptr));
+        pushButton_9->setText(QApplication::translate("MainWindow", "7", nullptr));
+        pushButton_13->setText(QApplication::translate("MainWindow", "0", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "3", nullptr));
+        pushButton_7->setText(QApplication::translate("MainWindow", "6", nullptr));
+        pushButton_11->setText(QApplication::translate("MainWindow", "9", nullptr));
+        pushButton_15->setText(QApplication::translate("MainWindow", "=", nullptr));
+        pushButton_4->setText(QApplication::translate("MainWindow", "+", nullptr));
+        pushButton_8->setText(QApplication::translate("MainWindow", "-", nullptr));
+        pushButton_12->setText(QApplication::translate("MainWindow", "*", nullptr));
+        pushButton_16->setText(QApplication::translate("MainWindow", "/", nullptr));
+        pushButton_2->setText(QApplication::translate("MainWindow", "2", nullptr));
+        pushButton_6->setText(QApplication::translate("MainWindow", "5", nullptr));
+        pushButton_10->setText(QApplication::translate("MainWindow", "8", nullptr));
+        pushButton_14->setText(QApplication::translate("MainWindow", "CE", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_QMAINWINDOW_H
